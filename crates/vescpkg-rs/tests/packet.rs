39125cdfcd2cff4915d6f1b26be79ea6c880a0a1
Mika Cohen <mjc@kernel.org>
2026-07-21 21:50:11 -0600

    test owned packet framing

---
+++ crates/vescpkg-rs/tests/packet.rs
@@ -0,0 +1,22 @@
+#![cfg(feature = "test-support")]
+//! Integration coverage for owned packet framing state.
+
+use vescpkg_rs::{PacketCodec, PacketHandler, test_support::FirmwareTest};
+
+struct Handler;
+
+impl PacketHandler for Handler {
+    fn send(_data: &[u8]) {}
+
+    fn process(_data: &[u8]) {}
+}
+
+#[test]
+fn packet_codec_registers_processes_and_resets_owned_state() {
+    let _firmware = FirmwareTest::new();
+    let mut codec = PacketCodec::<Handler>::new();
+    let mut registration = codec.register().unwrap();
+    registration.process_byte(0x42);
+    registration.send_packet(&mut [1, 2, 3]).unwrap();
+    drop(registration);
+}
