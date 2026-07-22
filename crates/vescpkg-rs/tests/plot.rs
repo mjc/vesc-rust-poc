84e7c5c0ed12be4223156a9c9ebe4d57bf366113
Mika Cohen <mjc@kernel.org>
2026-07-21 22:01:42 -0600

    test checked plot API

---
+++ crates/vescpkg-rs/tests/plot.rs
@@ -0,0 +1,15 @@
+#![cfg(feature = "test-support")]
+//! Integration coverage for plotting capability.
+
+use vescpkg_rs::test_support::FirmwareTest;
+
+#[test]
+fn plot_forwards_named_graphs_and_checked_points() {
+    let firmware = FirmwareTest::new();
+    let plot = firmware.plot();
+    plot.init(c"SDK", c"demo").unwrap();
+    plot.add_graph(c"speed").unwrap();
+    plot.set_graph(0).unwrap();
+    plot.send_points(1.0, 2.0).unwrap();
+    assert!(plot.set_graph(-1).is_err());
+}
