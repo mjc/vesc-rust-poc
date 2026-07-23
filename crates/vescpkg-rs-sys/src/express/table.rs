378a02026a3f1061848843232ae789fbe9c25ca4
Mika Cohen <mjc@kernel.org>
2026-07-22 22:52:11 -0600

    feat(express): expose slot metadata

--- crates/vescpkg-rs-sys/src/express/table.rs
+++ crates/vescpkg-rs-sys/src/express/table.rs
@@ -191,6 +191,110 @@ impl ExpressSlot {
     pub const fn index(self) -> usize {
         self as usize
     }
+
+    /// Return the originating C declaration name for this slot.
+    pub const fn name(self) -> &'static str {
+        match self {
+            Self::IfVersion => "if_version",
+            Self::LbmAddExtension => "lbm_add_extension",
+            Self::LbmSetErrorReason => "lbm_set_error_reason",
+            Self::LbmAddSymbolConst => "lbm_add_symbol_const",
+            Self::LbmGetSymbolByName => "lbm_get_symbol_by_name",
+            Self::LbmBlockCtxFromExtension => "lbm_block_ctx_from_extension",
+            Self::LbmUnblockCtx => "lbm_unblock_ctx",
+            Self::LbmUnblockCtxUnboxed => "lbm_unblock_ctx_unboxed",
+            Self::LbmGetCurrentCid => "lbm_get_current_cid",
+            Self::LbmSendMessage => "lbm_send_message",
+            Self::LbmPauseEvalWithGc => "lbm_pause_eval_with_gc",
+            Self::LbmContinueEval => "lbm_continue_eval",
+            Self::LbmEvalIsPaused => "lbm_eval_is_paused",
+            Self::LbmCons => "lbm_cons",
+            Self::LbmCar => "lbm_car",
+            Self::LbmCdr => "lbm_cdr",
+            Self::LbmListDestructiveReverse => "lbm_list_destructive_reverse",
+            Self::LbmCreateByteArray => "lbm_create_byte_array",
+            Self::LbmEncI => "lbm_enc_i",
+            Self::LbmEncU => "lbm_enc_u",
+            Self::LbmEncChar => "lbm_enc_char",
+            Self::LbmEncFloat => "lbm_enc_float",
+            Self::LbmEncU32 => "lbm_enc_u32",
+            Self::LbmEncI32 => "lbm_enc_i32",
+            Self::LbmEncSym => "lbm_enc_sym",
+            Self::LbmDecAsFloat => "lbm_dec_as_float",
+            Self::LbmDecAsU32 => "lbm_dec_as_u32",
+            Self::LbmDecAsI32 => "lbm_dec_as_i32",
+            Self::LbmDecChar => "lbm_dec_char",
+            Self::LbmDecStr => "lbm_dec_str",
+            Self::LbmDecSym => "lbm_dec_sym",
+            Self::LbmIsByteArray => "lbm_is_byte_array",
+            Self::LbmIsCons => "lbm_is_cons",
+            Self::LbmIsNumber => "lbm_is_number",
+            Self::LbmIsChar => "lbm_is_char",
+            Self::LbmIsSymbol => "lbm_is_symbol",
+            Self::LbmIsSymbolNil => "lbm_is_symbol_nil",
+            Self::LbmIsSymbolTrue => "lbm_is_symbol_true",
+            Self::LbmEncSymNil => "lbm_enc_sym_nil",
+            Self::LbmEncSymTrue => "lbm_enc_sym_true",
+            Self::LbmEncSymTerror => "lbm_enc_sym_terror",
+            Self::LbmEncSymEerror => "lbm_enc_sym_eerror",
+            Self::LbmEncSymMerror => "lbm_enc_sym_merror",
+            Self::LbmStartFlatten => "lbm_start_flatten",
+            Self::LbmFinishFlatten => "lbm_finish_flatten",
+            Self::FCons => "f_cons",
+            Self::FSym => "f_sym",
+            Self::FI => "f_i",
+            Self::FB => "f_b",
+            Self::FI32 => "f_i32",
+            Self::FU32 => "f_u32",
+            Self::FFloat => "f_float",
+            Self::FI64 => "f_i64",
+            Self::FU64 => "f_u64",
+            Self::FLbmArray => "f_lbm_array",
+            Self::SleepMs => "sleep_ms",
+            Self::SleepUs => "sleep_us",
+            Self::SystemTime => "system_time",
+            Self::TsToAgeS => "ts_to_age_s",
+            Self::SystemTimeTicks => "system_time_ticks",
+            Self::SleepTicks => "sleep_ticks",
+            Self::TimerTimeNow => "timer_time_now",
+            Self::TimerSecondsElapsedSince => "timer_seconds_elapsed_since",
+            Self::TimerSleep => "timer_sleep",
+            Self::Printf => "printf",
+            Self::Malloc => "malloc",
+            Self::Free => "free",
+            Self::Spawn => "spawn",
+            Self::RequestTerminate => "request_terminate",
+            Self::ShouldTerminate => "should_terminate",
+            Self::ThreadSetPriority => "thread_set_priority",
+            Self::GetArg => "get_arg",
+            Self::MutexCreate => "mutex_create",
+            Self::MutexLock => "mutex_lock",
+            Self::MutexUnlock => "mutex_unlock",
+            Self::SemCreate => "sem_create",
+            Self::SemWait => "sem_wait",
+            Self::SemSignal => "sem_signal",
+            Self::SemWaitTo => "sem_wait_to",
+            Self::SemReset => "sem_reset",
+        }
+    }
+
+    /// Return the pinned ABI kind for this slot.
+    pub const fn kind(self) -> ExpressSlotKind {
+        match self {
+            Self::IfVersion
+            | Self::LbmEncSymNil
+            | Self::LbmEncSymTrue
+            | Self::LbmEncSymTerror
+            | Self::LbmEncSymEerror
+            | Self::LbmEncSymMerror => ExpressSlotKind::Scalar,
+            _ => ExpressSlotKind::Function,
+        }
+    }
+
+    /// Return whether this slot contains a nullable function pointer.
+    pub const fn is_callable(self) -> bool {
+        matches!(self.kind(), ExpressSlotKind::Function)
+    }
 }
 
 /// Return the pinned kind of an Express slot, if it is in the v1 table.
